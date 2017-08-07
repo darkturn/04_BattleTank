27041d7e3cdb4cbff5ad3039082dd952261a53a0
darkturn <d_darkturn@yahoo.it>
2017-08-07 15:47:28 +0200

    Aggiunto il metodo BeginPlay() al tankplayercontrolled.cpp e loggato all'avvio se il player controller sta possedendo il tank o no.

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -2,6 +2,20 @@
 
 #include "TankPlayerController.h"
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank) 
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Il Player Controller non sta possedendo un tank"))
+	}
+	else 
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Il Player Controller sta possedendo: %s"), *(ControlledTank->GetName()))
+	}
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
