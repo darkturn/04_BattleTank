20f65f018e575eea163a01ee155f2afab7276755
darkturn <d_darkturn@yahoo.it>
2017-08-08 20:40:44 +0200

    Creata nuova classe C++: TankAimingComponent (di tipo Actor Component) e spostato il log di dove colpisce il mirino nel .cpp di quest'ultimo.

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "TankAimingComponent.h"
 #include "GameFramework/Pawn.h"
 #include "Tank.generated.h"
 
@@ -14,18 +15,19 @@ class BATTLETANK_API ATank : public APawn
 
 
 protected:
+	UTankAimingComponent* TankAimingComponent = nullptr;
+
+public:
+	void AimAt(FVector HitLocation);
+
+private:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
-public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
-	void AimAt(FVector HitLocation);
-private:
 	// Sets default values for this pawn's properties
 	ATank();
 	
