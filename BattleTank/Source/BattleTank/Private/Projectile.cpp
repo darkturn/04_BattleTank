f87a495d2e1cc1c04d77f729f7749379f307c66e
darkturn <d_darkturn@yahoo.it>
2017-08-13 00:53:17 +0200

    ...

--- BattleTank/Source/BattleTank/Private/Projectile.cpp
+++ BattleTank/Source/BattleTank/Private/Projectile.cpp
@@ -9,8 +9,6 @@ AProjectile::AProjectile()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(FName("Projectile Movement"));
-	ProjectileMovement->bAutoActivate = false;
 }
 
 // Called when the game starts or when spawned
--- BattleTank/Source/BattleTank/Public/Projectile.h
+++ BattleTank/Source/BattleTank/Public/Projectile.h
@@ -4,7 +4,6 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
-#include "GameFramework/ProjectileMovementComponent.h"
 #include "Projectile.generated.h"
 
 UCLASS()
@@ -16,13 +15,14 @@ public:
 	// Sets default values for this actor's properties
 	AProjectile();
 
+protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-private:	
-
-	UProjectileMovementComponent* ProjectileMovementComponent = nullptr;
+	
+	
 };
