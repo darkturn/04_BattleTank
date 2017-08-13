0f7789d013f11be6f6190e6bc37c28344f57caa6
darkturn <d_darkturn@yahoo.it>
2017-08-13 15:29:16 +0200

    Il proiettile adesso viene sparato alla velocità desiderata e viene mostrato nell'output log.

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -48,15 +48,14 @@ void ATank::AimAt(FVector HitLocation)
 
 void ATank::Fire()
 {
-	auto Time = GetWorld()->GetTimeSeconds();
-	UE_LOG(LogTemp, Warning, TEXT("%f: Il tank spara"), Time)
-
 	if (!Barrel) { return; }
 
 	//Spawna un proiettile alla posizione del socket sul barrel
-	GetWorld()->SpawnActor<AProjectile>(
+	auto Projectile = GetWorld()->SpawnActor<AProjectile>(
 		ProjectileBlueprint,
 		Barrel->GetSocketLocation(FName("Projectile")),
-		Barrel->GetSocketRotation(FName("Projectile")));
+		Barrel->GetSocketRotation(FName("Projectile"))
+	);
+	Projectile->LaunchProjectile(LaunchSpeed);
 }
 
