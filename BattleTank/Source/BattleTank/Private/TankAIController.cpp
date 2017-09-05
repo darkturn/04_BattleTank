04051401cce5b413d072865524b5ab3fb8515ff3
darkturn <d_darkturn@yahoo.it>
2017-09-05 20:31:05 +0200

    Sezione completata. Cambiati i mesh del Mortar e aggiunto noise al paesaggio.

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -37,7 +37,7 @@ void ATankAIController::Tick(float DeltaTime)
 	auto PlayerTank = GetWorld()->GetFirstPlayerController()->GetPawn();
 	auto ControlledTank = GetPawn();
 
-	if (!ensure(PlayerTank && ControlledTank)) { return; }
+	if (!PlayerTank && !ControlledTank) { return; }
 	
 		//Muoversi verso il giocatore
 		MoveToActor(PlayerTank, AcceptanceRadius);
